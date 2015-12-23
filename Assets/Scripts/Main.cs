6a195faeb5f3c1f687173782b205aabd9f58ac83
Jackson <jacksonlango@gmail.com>
2015-12-22 23:37:14 -0800

    basic carrot pulling and throwing game with ui elements

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+    private UIButtonInput left_button;
+    private UIButtonInput right_button;
+    private UIButtonInput jump_button;
+    private UIButtonInput action_button;
+
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+        var canvas = GameObject.Find("Canvas");
+        this.left_button = GameObject.Find("Left Button").GetComponent<UIButtonInput>();
+        this.right_button = GameObject.Find("Right Button").GetComponent<UIButtonInput>();
+        this.jump_button = GameObject.Find("Jump Button").GetComponent<UIButtonInput>();
+        this.action_button = GameObject.Find("Action Button").GetComponent<UIButtonInput>();
+
+        this.player = GameObject.Find("Player").GetComponent<Player>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.left_button.pressed)
+            this.player.MoveLeft();
+        if (this.right_button.pressed)
+            this.player.MoveRight();
+        if (this.jump_button.getSinglePress())
+            this.player.Jump();
+        if (this.action_button.getSinglePress())
+            this.player.Action();
+	}
+}
---
+++ Assets/Scripts/UIButtonInput.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIButtonInput : MonoBehaviour {
+    
+    public bool pressed = false;
+
+    public bool getSinglePress()
+    {
+        if (this.pressed)
+        {
+            this.pressed = false;
+            return true;
+        }
+        return false;
+    }
+
+    private EventTrigger trigger;
+
+    void Start()
+    {
+        this.trigger = this.gameObject.AddComponent<EventTrigger>();
+        // this.AddListener(EventTriggerType.PointerDown, (eventData) => { Down(); });
+        // this.AddListener(EventTriggerType.PointerUp, (eventData) => { Up(); });
+        this.AddListener(EventTriggerType.PointerEnter, (eventData) => { Down(); });
+        this.AddListener(EventTriggerType.PointerExit, (eventData) => { Up(); });
+    }
+
+    private void AddListener(EventTriggerType type, UnityEngine.Events.UnityAction<BaseEventData> call)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = type;
+        entry.callback.AddListener(call);
+        this.trigger.triggers.Add(entry);
+    }
+
+    private void Down()
+    {
+        this.pressed = true;
+    }
+
+    private void Up()
+    {
+        this.pressed = false;
+    }
+}
